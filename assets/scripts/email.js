846c7223e95d1e87d0dbf5b9399c04978d1e2dc0
Linda <oneill.linda21@gmail.com>
2018-10-02 13:47:02 -0500

    add confirmation redirect

--- assets/scripts/email.js
+++ assets/scripts/email.js
@@ -1,20 +1,23 @@
-$('#mbr-form').submit(function(e) {
-  var name = document.getElementById('#name-form1-3'),
-  email = document.getElementById('#email-form1-3'),
-  subject = document.getElementById('#subject-form1-3'),
-  message = document.getElementById('#message-form1-3');
+$(document).ready(function() {
+  $('#mbr-form').submit(function(e) {
+    var name    = document.getElementById('name-form1-3')
+    var email   = document.getElementById('email-form1-3')
+    var subject = document.getElementById('subject-form1-3')
+    var message = document.getElementById('message-form1-3')
 
-  if (!name.value || !email.value || !subject.value || !message.value ) {
-    alertify.error('Please enter a value')
-  } else {
-    $ajax({
-      url: "https://formspree.io/oneill.linda21@gmail.com",
-      method: "post",
-      data: $(this).serialize(),
-      dataType: "json"
-    });
-    e.preventDefault()
-    $(this).get(0)reset()
-    alertify.success('Message sent!')
-  }
+    if (!name.value || !email.value || !subject.value || !message.value) {
+      alertify.error("Please make a valid entry");
+      return false;
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '//formspree.io/oneill.linda21@gmail.com',
+        data: $('#mbr-form').serialize(),
+        datatype: 'json'
+      });
+      e.preventDefault();
+      $(this).get(0).reset();
+      alertify.success("Message sent!");
+    }
+  });
 });
